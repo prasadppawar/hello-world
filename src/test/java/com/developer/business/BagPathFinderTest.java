fb2817350267b3ce48489581e2976fe89d617dee
PrasadPawar <prasadpawarp3@gmail.com>
2016-05-23 16:24:44 -0400

    One more commit

--- src/test/java/com/developer/business/BagPathFinderTest.java
+++ src/test/java/com/developer/business/BagPathFinderTest.java
@@ -1,10 +1,7 @@
 package com.developer.business;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.developer.model.FlightDeparture;
 
